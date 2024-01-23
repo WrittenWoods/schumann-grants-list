7ade76cc743bf7759260ae5c0974d4c9106b4bc7
kelseyarmstrong <154465150+kelseyarmstrong@users.noreply.github.com>
2024-01-22 22:59:36 -0500

    Added icons for tags/query types
    Added css for tag font;
    added ts file for icon globals;
    changed structure in Criteria and Results to show corresponding icons for tags;

---
+++ src/helpers/generalOptions.ts
@@ -0,0 +1,19 @@
+// Additional Globals
+
+	iconLocation = "fa-solid fa-location-dot"
+	iconGrantType = "fa-solid fa-calendar-day"
+	iconFundingType = "fa-solid fa-gear"
+	iconProgramArea = "fa-solid fa-folder"
+	iconStrategy = "fa-solid fa-bullseye"
+	iconDonor = "fa-solid fa-hand-holding-dollar"
+	iconKeyword = "fa-solid fa-magnifying-glass"
+	iconDate = "fa-solid fa-calendar-days"
+	iconAmount = "fa-solid fa-circle-dollar-to-slot"
+	iconOrg = "fa-solid fa-house"
+
+
+
+
+
+
+
