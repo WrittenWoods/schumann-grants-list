4f5f9437ee12b3c3ebae3410375229d135044680
Paul <paul.b.dowling@gmail.com>
2024-01-26 19:04:36 +0800

    Refactored App/Results/Nav/ColumnHead to avoid passing results down the hierarchy

--- src/components/ColumnHead.tsx
+++ src/components/ColumnHead.tsx
@@ -1,20 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { SortableColumns, SortStatus } from '../helpers/enums';
+import { SortStatus } from '../helpers/enums';
 
-function ColumnHead({ name, filteredResults, setFilteredResults, sortedColumn, setSortedColumn }) {
+function ColumnHead({ name, isSorted, setSortedColumn }:{name:string, isSorted: boolean, setSortedColumn:(column:string, reversed:boolean) => void}) {
+  const [ reversed, setReversed ] = useState<boolean>(false);
 
-  const [reversed, setReversed] = useState(true)
+  useEffect(() => {
+    setReversed( isSorted ? false : true)
+  }, [isSorted]);
 
-  const sortFunctions = sortFunctionsArray(name)
 
-  function sortFunctionsArray(name) {
-    if (name === SortableColumns.Amount) { return [dateCompare, stringCompare, amountCompare] }
-    if (name === SortableColumns.Result) { return [amountCompare, dateCompare, stringCompare] }
-    return [amountCompare, stringCompare, dateCompare]
-  }
-
-  function stringCompare(a, b) {
+  function stringCompare(a:any, b:any) {
     let compare = a.orgName.localeCompare(b.orgName)
 
     if (compare === 0) {
@@ -30,17 +26,17 @@ function ColumnHead({ name, filteredResults, setFilteredResults, sortedColumn, s
     return compare
   }
 
-  function dateCompare(a, b) {
+  function dateCompare(a:any, b:any) {
       let yearCompare = Math.sign(a.year - b.year)
       return yearCompare === 0 ? Math.sign(a.month - b.month) : yearCompare
   }
 
-  function amountCompare(a, b) {
+  function amountCompare(a:any, b:any) {
       return Math.sign(a.amount - b.amount)
   }
 
   function renderSortButton() {
-    if (sortedColumn === name) {
+    if (isSorted) {
       return (
         // <button onClick={e => handleSortButton()}>
         <>
@@ -50,21 +46,19 @@ function ColumnHead({ name, filteredResults, setFilteredResults, sortedColumn, s
     }
   }
 
+
   function handleSortButton() {
-    let newResults = filteredResults
-    setReversed(!reversed)
-    for (let i = 0; i < sortFunctions.length; i++) { newResults = newResults.sort(sortFunctions[i]) }
-    if (!reversed) { newResults = newResults.reverse() }
-    setFilteredResults([...newResults])
-    setSortedColumn(name)
+    let rev = !reversed
+    setSortedColumn({column: name, reversed: rev});
+    setReversed(rev)
   }
 
   function activeSortClass() {
-    return (sortedColumn !== name ? 'db__results-sorted-inactive' : 'db__results-sorted-active')  
+    return (isSorted ? 'db__results-sorted-inactive' : 'db__results-sorted-active')  
   }
 
   function sortDirection() {
-    return (sortedColumn !== name 
+    return (!isSorted 
         ? ''
         : reversed ? 'db__results-sorted-ascending' : 'db__results-sorted-descending'
     ) 
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -7,14 +7,13 @@
 
 */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Result from './Result';
 import Nav from './Nav';
 import PaginationBar from './PaginationBar';
-import { generateTallies } from '../helpers/generateTallies';
 
-function Results( { loadedData, userInputs, filteredResults, setFilteredResults } ) {
+function Results( { sortedColumn, userInputs, setSortedColumn, filteredResults } ) {
 
   const [pageStart, setPageStart] = useState(0)
   const [pageEnd, setPageEnd] = useState(10)
@@ -22,117 +21,6 @@ function Results( { loadedData, userInputs, filteredResults, setFilteredResults 
   // The functions below are named according to which properties of the userInputs object they identify.
   // If the function returns true at an index of loadedData, the result at that index is displayed.
 
-  useEffect(() => {
-    setFilteredResults(filterGrants(loadedData, userInputs))
-  }, [userInputs] )
-
-  function dateMatch(data, inputDates) {
-    let grantYear = data.year
-    let grantMonth = data.month
-
-    if (inputDates.minYear === grantYear) {
-      if (grantMonth >= inputDates.minMonth) { return true }
-    } else if (inputDates.maxYear === grantYear) {
-      if (grantMonth <= inputDates.maxMonth) { return true }
-    } else if (inputDates.minYear < grantYear && grantYear < inputDates.maxYear) {
-      return true
-    }
-
-    return false
-
-  }
-
-  function amountMatch(amount, minVal, maxVal) {
-    let aboveMin = false, belowMax = false
-
-    if (minVal.trim() === "" && maxVal.trim() === "") { return true }
-    if (Number(minVal) || minVal === "0") { aboveMin = (amount >= Number(minVal)) }
-    if (Number(maxVal) || maxVal === "0") { belowMax = (amount <= Number(maxVal)) }
-      
-    if (minVal.trim() === "" || maxVal.trim() === "") {
-      return aboveMin || belowMax
-    } else {
-      return aboveMin && belowMax
-    }
-  }
-
-  function locationMatch(data, inputs) {
-    let city = data.orgCity
-    let state = data.orgState
-    let cityMatch = false
-    let stateMatch = false
-
-    if (inputs.orgCities.includes(city)) {
-      cityMatch = true
-    }
-
-    if (inputs.orgStates.includes(state)) {
-      stateMatch = true
-    }
-
-    if (inputs.orgStates.length === 0 && inputs.orgCities.length === 0) {
-      return true
-    }
-
-    if (inputs.orgStates.length === 0 || inputs.orgCities.length === 0) {
-      return cityMatch || stateMatch
-    } else {
-      return cityMatch && stateMatch
-    }
-
-  }
-
-  function keywordMatch(data, inputs) {
-    let anyTerms = inputs.anyTerms
-    let queries = inputs.searchQueries
-    let toMatch = [data.orgName, data.description, data.donor, data.fundingType, data.grantType, data.orgCity, data.programArea, data.strategy, data.strategy2]
-    let matchedAny = false
-    let matchedAll = true
-
-    if (queries.length === 0) { return true }
-
-    for (let i = 0; i < queries.length; i++) {
-      if (toMatch.some( (s) => s.toLowerCase().includes(queries[i].toLowerCase()) )) {
-        matchedAny = true
-      } else {
-        matchedAll = false
-      }
-    }
-
-    return anyTerms ? matchedAny : matchedAll
-
-  }
-
-  function inputMatch(inputArray, arg) {
-    if (inputArray === undefined || inputArray.length === 0) { return true }
-    return inputArray.includes(arg.trim())
-  }
-
-  // Checks a particular grant for correspondence with userInputs.
-
-  function grantMatch(data, inputs) {
-    let match = 
-      dateMatch(data, inputs)
-      && amountMatch(data.amount, inputs.minVal, inputs.maxVal)
-      && inputMatch(inputs.orgNames, data.orgName)
-      && locationMatch(data, inputs)
-      && inputMatch(inputs.grantTypes, data.grantType)
-      && inputMatch(inputs.fundingTypes, data.fundingType) 
-      && inputMatch(inputs.programAreas, data.programArea) 
-      && (inputMatch(inputs.strategies, data.strategy) || (inputMatch(inputs.strategies, data.strategy2)))
-      && inputMatch(inputs.donors, data.donor)
-      && keywordMatch(data, inputs)
-
-    return match
-  }
-
-  // Iterates through loadedData array to check for matches with userInputs.
-
-  function filterGrants(data, inputs) {
-    let filteredResults = [...data]
-    filteredResults = filteredResults.filter((x) => grantMatch(x, inputs))
-    return filteredResults
-  }
 
   // Uses the array generated from the filterGrants function to render Result components.
 
@@ -140,8 +28,8 @@ function Results( { loadedData, userInputs, filteredResults, setFilteredResults 
     <>
       <nav className="db__results-nav">
         <Nav 
-          filteredResults={filteredResults}
-          setFilteredResults={setFilteredResults}
+          sortedColumn={sortedColumn}
+          setSortedColumn={setSortedColumn}
         />
       </nav>
       {[...filteredResults].slice(pageStart, pageEnd).map( (individualGrant, n) =>
